9b631116a8c906c611c13acc3d263aa3432a2dbf
Deepak <deepak.kuamr@wowmakers.com>
2013-05-22 15:08:07 +0530

    use app and db roles on servers

--- cookbooks/crowdstudio/recipes/default.rb
+++ cookbooks/crowdstudio/recipes/default.rb
@@ -14,22 +14,22 @@
 #   action :nothing
 # end.run_action(:run) if node['platform_family'] == "debian"
 
-# apt_package 'python-software-properties software-properties-common' do
-#   action :install
-# end
+apt_package 'python-software-properties software-properties-common' do
+  action :install
+end
 
-# execute 'add-apt-repository ppa:chris-lea/node.js' do
-#   action :nothing
-# end.run_action(:run)
+execute 'add-apt-repository ppa:chris-lea/node.js' do
+  action :nothing
+end.run_action(:run)
 
-# execute "apt-get update" do
-#   ignore_failure true
-#   action :nothing
-# end.run_action(:run) if node['platform_family'] == "debian"
+execute "apt-get update" do
+  ignore_failure true
+  action :nothing
+end.run_action(:run) if node['platform_family'] == "debian"
 
-# apt_package 'build-essential libmagickcore-dev imagemagick libxml2-dev libxslt1-dev git-core curl nodejs htop' do
-#   action :install
-# end
+apt_package 'build-essential libmagickcore-dev imagemagick libxml2-dev libxslt1-dev git-core curl nodejs htop' do
+  action :install
+end
 
 # gem_package 'ruby-shadow' do
 #   action :nothing
