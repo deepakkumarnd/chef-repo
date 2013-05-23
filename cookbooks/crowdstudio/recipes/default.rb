3e9b35d6aba37f6c2f1abda3a08e5fbe7ce5d484
root <root@domU-12-31-39-0F-8C-2E.compute-1.internal>
2013-05-23 10:11:05 +0000

    create deploy user and apps directory

--- cookbooks/crowdstudio/recipes/default.rb
+++ cookbooks/crowdstudio/recipes/default.rb
@@ -36,13 +36,25 @@ end
 # end.run_action(:install)
 
 # create a deploy group
-# group 'deploy' do
-# end
+group 'deploy' do
+  action :create
+end
 
 # create a deploy user
 
 user 'deploy' do
+  comment 'Deploy user'
   username 'deploy'
+  home '/home/deploy'
+  supports :manage_home => true
   shell '/bin/bash'
+  gid 'deploy'
+  action :create
+end
+
+directory 'apps' do
+  user 'deploy'
+  group 'deploy'
+  mode 00755
   action :create
 end
