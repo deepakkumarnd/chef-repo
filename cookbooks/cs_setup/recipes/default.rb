e12e4be1f0b44f99bf351f52310673ceaf55bbd8
Deepak Kumar <deepak.kumar@wowmakers.com>
2013-05-25 08:56:38 +0000

    sourcing rvm in /etc/bash.bashrc

--- cookbooks/cs_setup/recipes/default.rb
+++ cookbooks/cs_setup/recipes/default.rb
@@ -1,1 +1,6 @@
 # source source /etc/profile.d/rvm.sh in /etc/bash.bashrc
+
+execute 'echo "source /etc/profile.d/rvm.sh" >> /etc/bash.bashrc' do
+  action :run
+  not_if { File.read('/etc/bash.bashrc').match('source /etc/profile.d/rvm.sh') }
+end.run_action(:run)
