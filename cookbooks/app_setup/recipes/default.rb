7c49f429c78b12b501b7bea9d45d472de2a899fa
Deepak <deepak.kuamr@wowmakers.com>
2013-05-29 17:13:33 +0530

    add redis monitor conf template

--- cookbooks/app_setup/recipes/default.rb
+++ cookbooks/app_setup/recipes/default.rb
@@ -5,6 +5,11 @@ execute 'echo "source /etc/profile.d/rvm.sh" >> /etc/bash.bashrc' do
   not_if { File.read('/etc/bash.bashrc').match('source /etc/profile.d/rvm.sh') }
 end.run_action(:run)
 
+# monit configuration files
 template '/etc/monit/conf.d/nginx.conf' do
   source 'nginx.erb'
+end
+
+template '/etc/monit/conf.d/redis.conf' do
+  source 'redis.erb'
 end
