3b0da7f57c90c316efe368717e253bb159d9fc5f
Deepak <deepak.kuamr@wowmakers.com>
2013-05-29 15:29:08 +0530

    monit setup for nginx

--- cookbooks/app_setup/recipes/default.rb
+++ cookbooks/app_setup/recipes/default.rb
@@ -4,3 +4,7 @@ execute 'echo "source /etc/profile.d/rvm.sh" >> /etc/bash.bashrc' do
   action :run
   not_if { File.read('/etc/bash.bashrc').match('source /etc/profile.d/rvm.sh') }
 end.run_action(:run)
+
+template '/etc/monit/conf.d/nginx.conf' do
+  source 'nginx.erb'
+end
