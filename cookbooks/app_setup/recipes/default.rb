8c67cf9f94734de19e0c4e63cbcdf9de51cb6ac8
Deepak <deepak.kuamr@wowmakers.com>
2013-06-03 11:47:29 +0530

    install ruby using rvm

--- cookbooks/app_setup/recipes/default.rb
+++ cookbooks/app_setup/recipes/default.rb
@@ -5,6 +5,10 @@ execute 'echo "source /etc/profile.d/rvm.sh" >> /etc/bash.bashrc' do
   not_if { File.read('/etc/bash.bashrc').match('source /etc/profile.d/rvm.sh') }
 end.run_action(:run)
 
+execute 'rvm install 1.9.3' do
+  action :run
+end.run_action(:run)
+
 # monit configuration files
 template '/etc/monit/conf.d/nginx.conf' do
   source 'nginx.erb'
