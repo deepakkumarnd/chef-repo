bde42a70d78de6e5fb5d963ca675ec85d8c60221
Deepak <deepak.kuamr@wowmakers.com>
2013-07-19 14:00:31 +0530

    install nginx

--- cookbooks/install_ruby/recipes/default.rb
+++ cookbooks/install_ruby/recipes/default.rb
@@ -1,6 +1,6 @@
-execute 'source /etc/bash.bashrc' do
-  action :run
-end.run_action(:run)
+# execute 'source /etc/bash.bashrc' do
+#   action :run
+# end.run_action(:run)
 
 execute 'rvm install 2.0.0' do
   action :run
